86e1a4c2a049551d144e56b24f286ee72980b60d
YannMallegol <yann.mallegol@isen-bretagne.fr>
2015-07-16 14:06:48 +0200

    Update readfile.js

--- readfile.js
+++ readfile.js
@@ -7,7 +7,17 @@ fs.readFile('file.txt', function (err, data) {
 
   if (err) throw err;
 
-  console.log(data);
+  //console.log(data);
+
+var _byteArray = new Uint8Array(data);
+//var newrray = new String(_byteArray, "");
+console.log(_byteArray);
+console.log(_byteArray.length);
+
+for(var i = 0; _byteArray.length; i++){
+  console.log(String.fromCharCode(_byteArray[i]));
+}
+
+
 
 });
-
